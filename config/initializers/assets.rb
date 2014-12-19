3abdb81ca62e063f6d259a9c0886c92d4095e322
AliOsm <AliOsm1997@gmail.com>
2014-12-20 00:34:38 +0200

    test mruby upload

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,4 +6,3 @@ Rails.application.config.assets.version = '1.1'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( ace.js jquery.js events.js webruby.js )
